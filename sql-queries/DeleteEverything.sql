0f75b9800f12a3d84ea599596f653710d8702bce
Matthew Jeorrett <matthew@clearskylogic.com>
2023-06-22 21:37:46 +0100

    api: update delete everything script

--- sql-queries/DeleteEverything.sql
+++ sql-queries/DeleteEverything.sql
@@ -5,8 +5,10 @@ DELETE FROM FeatureTag;
 DELETE FROM Ticket;
 DELETE FROM UserStory;
 DELETE FROM Feature;
+DELETE FROM LoggedTime;
+DELETE FROM FloatPerson;
 
 DELETE FROM Tag;
 
 UPDATE Project
-SET LinksContinuationToken = '', WorkItemsContinuationToken = ''
+SET LinksContinuationToken = '', WorkItemsContinuationToken = '', LoggedTimeHasBeenSyncedUntil = NULL
