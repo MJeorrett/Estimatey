86468cc76450b041eb900f27d842423fda420940
Matthew Jeorrett <matthew@clearskylogic.com>
2023-06-23 16:38:11 +0100

    api: add NowDateOnly method

--- estimatey-api/Estimatey.Application/Common/Interfaces/IDateTimeService.cs
+++ estimatey-api/Estimatey.Application/Common/Interfaces/IDateTimeService.cs
@@ -3,4 +3,6 @@ namespace Estimatey.Application.Common.Interfaces;
 public interface IDateTimeService
 {
     DateTime Now { get; }
+
+    DateOnly NowDateOnly { get; }
 }
--- estimatey-api/Estimatey.Infrastructure/DateTimes/DateTimeService.cs
+++ estimatey-api/Estimatey.Infrastructure/DateTimes/DateTimeService.cs
@@ -5,4 +5,6 @@ namespace Estimatey.Infrastructure.DateTimes;
 public class DateTimeService : IDateTimeService
 {
     public DateTime Now => DateTime.Now;
+
+    public DateOnly NowDateOnly => DateOnly.FromDateTime(DateTime.Now);
 }
